02075803b15b04ec8646b3ad3db6716dc6f1b996
Douglas Queiroz <152705113+dev-queiroz@users.noreply.github.com>
2024-06-25 13:38:55 -0300

    Formulários estilizados

--- views/admins.php
+++ views/admins.php
@@ -3,24 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Administração</title>
 </head>
 <body>
     <main>
-        <form action="repositoriosForAdmin/adminRepositorio.php" method="post">
-            <h1>Página do Administrador</h1>
-            <!-- Nesta parte, serão implementadas coletas de requisições para as funções de "adminRepositorio.php" -->
-            <label for="checarUsuarioCPF">Checagem através do CPF:</label>
-            <input type="int" id="checarUsuarioCPF" name="checarUsuarioCPF" required>
-            <button type="submit" name="checarUsuarioCPF">Checar CPF</button>
-            <br><br>
-            <button type="submit" name="verTabelaCompleta">Verificar Tabela Completa</button>
-            <br><br>
-            <label for="banirUsuarioCPF">Banir usuário através do CPF:</label>
-            <input type="int" id="banirUsuarioCPF" name="banirUsuarioCPF" required>
-            <button type="submit" name="banirUsuarioCPF">Banir</button>
-        </form>
+        <div class="card">
+            <form action="repositoriosForAdmin/adminRepositorio.php" method="post">
+                <h1>Página do Administrador</h1>
+                <label for="checarUsuarioCPF">Checagem através do CPF:</label>
+                <input type="int" id="checarUsuarioCPF" name="checarUsuarioCPF" required>
+                <button type="submit" name="checarUsuarioCPF">Checar CPF</button>
+                <br><br>
+                <button type="submit" name="verTabelaCompleta">Verificar Tabela Completa</button>
+                <br><br>
+                <label for="banirUsuarioCPF">Banir usuário através do CPF:</label>
+                <input type="int" id="banirUsuarioCPF" name="banirUsuarioCPF" required>
+                <button type="submit" name="banirUsuarioCPF">Banir</button>
+            </form>
+        </div>
     </main>
 </body>
 </html>
--- views/registerform.php
+++ views/registerform.php
@@ -3,30 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Registro</title>
 </head>
 <body>
-    <h1>Registro</h1>
-    <form action="register.php" method="post">
-        <label for="nome">Nome:</label>
-        <input type="text" name="nome" id="nome" required>
+    <div class="card">
+        <form action="register.php" method="post">
+            <h1>Registro</h1>
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome" id="nome" required>
 
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email" required>
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email" required>
 
-        <label for="cpf">CPF:</label>
-        <input type="number" name="cpf" id="cpf" required>
+            <label for="cpf">CPF:</label>
+            <input type="number" name="cpf" id="cpf" required>
 
-        <label for="telefone">Telefone:</label>
-        <input type="number" name="telefone" id="telefone" required>
+            <label for="telefone">Telefone:</label>
+            <input type="number" name="telefone" id="telefone" required>
 
-        <label for="data">Data de Nascimento:</label>
-        <input type="date" name="data" id="data" required>
+            <label for="data">Data de Nascimento:</label>
+            <input type="date" name="data" id="data" required>
 
-        <label for="senha">Senha:</label>
-        <input type="password" name="senha" id="senha" required>
+            <label for="senha">Senha:</label>
+            <input type="password" name="senha" id="senha" required>
 
-        <button type="submit">Registrar</button>
-    </form>
+            <button type="submit">Registrar</button>
+        </form>
+    </div>
 </body>
 </html>

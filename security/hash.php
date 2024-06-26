522d1458b2cef53de9856a9e29d93ade76f1e734
Douglas Queiroz <152705113+dev-queiroz@users.noreply.github.com>
2024-06-26 16:50:42 -0300

    HASH ATUALIZE

--- security/hash.php
+++ security/hash.php
@@ -2,3 +2,4 @@
 
 $admins = password_hash('senha_admin', PASSWORD_DEFAULT);
 $moderators = password_hash('senha_moderator', PASSWORD_DEFAULT);
+$products = password_hash('senha_moderator', PASSWORD_DEFAULT);

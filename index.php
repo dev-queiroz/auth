b5e101695d4d091bf8edaba583a8334742fa5662
Douglas Queiroz <152705113+dev-queiroz@users.noreply.github.com>
2024-06-28 13:27:21 -0300

    estilo

--- index.php
+++ index.php
@@ -8,7 +8,17 @@
 </head>
 <body>
     <header>
-        <h1>Xiaomi Brasil</h1>
+        <div>
+            <h1>Xiaomi Brasil</h1>
+        </div>
+        <div class="link">
+            <nav>
+                <ul>
+                    <li><a href="register.php">Registre-se e use a Tecnologia!</a></li>
+                    <li><a href="login.php">Já é cadastrado? Login</a></li>
+                </ul>
+            </nav>
+        </div>
     </header>
 
     <main>
@@ -19,43 +29,36 @@
                     <img src="images/xiaomi_note_13_pro.jpg" alt="Xiaomi Note 13 Pro">
                     <h3>Xiaomi Redmi Note 13 Pro</h3>
                     <p>Câmera principal de 200MP, tela AMOLED de 120Hz e bateria de 5100mAh com carregamento turbo de 67W, o melhor para quem busca inovação e conforto.</p>
-                    <button>Compre já!</button>
                 </div>
                 <div class="product-card">
                     <img src="images/xiaomi_note_12.jpg" alt="Xiaomi Note 12">
                     <h3>Xiaomi Redmi Note 12</h3>
                     <p>Tela AMOLED de 6,67 polegadas, câmera tripla de 50MP, bateria de 5000mAh e processador Snapdragon 4 Gen 1, com ultra desempenho em jogos mobile.</p>
-                    <button>Compre já!</button>
                 </div>
                 <div class="product-card">
                     <img src="images/smartwatch_xiaomi_amazfit.jpg" alt="Smartwatch Xiaomi Amazfit">
                     <h3>Smartwatch Xiaomi Amazfit GTS 2 Mini</h3>
                     <p>Smartwatch leve e elegante com tela AMOLED, monitoramento de saúde 24/7, mais de 70 modos esportivos e bateria de até 14 dias, ideal para quem busca estilo e bem-estar.</p>
-                    <button>Compre já!</button>
                 </div>
                 <div class="product-card">
                     <img src="images/xiaomi_redmi_watch_3.jpg" alt="Xiaomi Redmi Watch 3">
                     <h3>Smartwatch Xiaomi Redmi Watch 3</h3>
                     <p>Mais de 100 modos esportivos, smartwatch com tela AMOLED de 1,75", monitoramento de saúde 24 horas e resistência à água de 5 ATM, especialista em custo-benefício.</p>
-                    <button>Compre já!</button>
                 </div>
                 <div class="product-card">
                     <img src="images/redmi_buds_5.jpg" alt="Redmi Buds 5">
                     <h3>Xiaomi Redmi Buds 5</h3>
                     <p>Nosso fone de ouvido sem fio oferece cancelamento de ruído ativo de até 46dB, bateria de até 40 horas e som imersivo com drivers de 10mm, tudo por um preço acessível.</p>
-                    <button>Compre já!</button>
                 </div>
                 <div class="product-card">
                     <img src="images/redmi_airdots_s.jpg" alt="Redmi Airdots S">
                     <h3>Xiaomi Redmi Airdots S</h3>
                     <p>Nosso fone de ouvido bluetooth possui boa qualidade de som, bateria de longa duração e preço acessível, ideal para quem busca praticidade no dia a dia.</p>
-                    <button>Compre já!</button>
                 </div>
                 <div class="product-card">
                     <img src="images/xiaomi_su7_5_blue.jpg" alt="Xiaomi SU7 Blue">
                     <h3>Xiaomi SU7</h3>
                     <p>O seu novo sedã elétrico de alto desempenho com design elegante, com autonomia de até 830 km, sistema HyperOS e preço competitivo. O novo sucesso no mercado de carros elétricos.</p>
-                    <button>Compre já!</button>
                 </div>
             </div>
         </section>
